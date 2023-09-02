6041e372057945df7285d73e3f29eb373931c091
Gustavo_Riedel <gustavorr272@gmail.com>
2023-09-02 07:14:38 -0300

    Mudancas de Modulo

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
-from tabuleiro import *
-from regras import *
+from Partida import *
 
-#Cria o deck
-deck = Deck()
-
-#Cria as colunas e distribui as cartas
-colunas = deck.mesa(face_up=False)
-
-for coluna in colunas:
-    if coluna:
-        coluna[-1].face_up = True
+inicia_jogo(Deck())
 
 
-#Imprime as Instruções
-print("Instruções:")
-print("Para mover cartas de uma coluna para outra digite o numero da coluna e depois o numero da carta")
-print("por fim digite o numero da coluna destino")
-print("Caso queira pegar cartas do monte digite 'm'")
 
-#Imprime a mesa
-imprime_tabuleiro(colunas)
-
-#Inicia o switch
-x = 0
 '''
-Aqui está o modelo do Switch que será usado no jogo
-while x != 'Fim':
-    print("Insira a sua jogada desejada:")
-    
-    x = input()
-    if (x == 'm'):
-        deck.monte(colunas)
-    if(check_x()):
-'''
+while True:
+    x = input("Insira sua jogada:\n")
+    if x == :
+        mov_
 
-deck.monte(colunas)
-imprime_tabuleiro(colunas)
+
+'''
--- tabuleiro.py
+++ tabuleiro.py
@@ -1,65 +1,21 @@
-import random
+from Partida import *
 
-#Classe de Carta
 
-class Card:
-    def __init__(self, valor, face_up = False):
-        self.valor = valor
-        self.face_up = face_up
 
-    def __str__(self):
-        return f"{self.valor}" if self.face_up else "x"
+
+
+#Inicia o switch
+x = 0
+'''
+Aqui está o modelo do Switch que será usado no jogo
+while x != 'Fim':
+    print("Insira a sua jogada desejada:")
     
+    x = input()
+    if (x == 'm'):
+        deck.monte(colunas)
+    if(check_x()):
+'''
 
-class Deck:
-    def __init__(self):
-        self.valores = 8 * ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        self.cards = [Card(valor) for valor in self.valores]
-        self.shuffle()
-
-    def shuffle(self):
-        random.shuffle(self.cards)
-        
-    def mesa(self, face_up = False):
-        #Colunas é como se fosse a mesa
-        colunas = []
-
-        #Como nosso objetivo é distribuir 6 cartas para 4 colunas, iremos fazer 2 for
-        for elem in range(4):
-            #Cria 1 coluna
-            coluna = []
-            for elem in range(6):
-                #Distribui 6 cartas
-                if self.cards:
-                    card = self.cards.pop()
-                    card.face_up = face_up
-                    coluna.append(card)
-            colunas.append(coluna)
-
-        #Distribui 5 cartas para 6 colunas
-        for elem in range(6):
-            #Cria 1 coluna
-            coluna = []
-            for elem in range(5):
-                #Distribui 6 cartas
-                if self.cards:
-                    card = self.cards.pop()
-                    card.face_up = face_up
-                    coluna.append(card)
-            colunas.append(coluna)
-
-        return colunas
-        
-    
-
-    def monte(self, colunas):
-        for coluna in colunas:
-            if self.cards:
-                #Removo a carta adicionada para não haver repetições
-                card = self.cards.pop()
-                card.face_up = True
-                coluna.append(card)
-
-def imprime_tabuleiro(colunas):
-    for i, coluna in enumerate(colunas, start=1):
-        print(f"Coluna {i}: {[str(card) for card in coluna]}")
+#deck.monte(colunas)
+#imprime_tabuleiro(colunas)
